7299e4377bf3525941e40a8c7079ae749be50f68
nourhan312 <nourhanaymanghonim@gmail.com>
2024-08-12 02:18:46 +0300

    dark them

--- facebookApp/app/src/main/java/com/example/facebookapp/MainActivity.java
+++ facebookApp/app/src/main/java/com/example/facebookapp/MainActivity.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+// For Dark Mode
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         setContentView(R.layout.activity_main);
+
+
     }
 }
