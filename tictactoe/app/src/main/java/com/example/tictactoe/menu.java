77d0cb83b8a8b5a7fa634508e36292a1ff312c06
nourhan312 <nourhanaymanghonim@gmail.com>
2024-08-22 05:08:54 +0300

    some of ui

--- tictactoe/app/src/main/java/com/example/tictactoe/menu.java
+++ tictactoe/app/src/main/java/com/example/tictactoe/menu.java
@@ -8,11 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class game extends AppCompatActivity {
+public class menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
