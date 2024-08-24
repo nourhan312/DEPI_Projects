bf6ae904f133920dff0cd945dff910dd73d09bad
nourhan312 <nourhanaymanghonim@gmail.com>
2024-08-25 01:59:36 +0300

    add splash screen

---
+++ tictactoe/app/src/main/java/com/example/tictactoe/splash.java
@@ -0,0 +1,25 @@
+package com.example.tictactoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        EdgeToEdge.enable(this);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 300);
+    }
+}
